7454eed54f8adb6df64326c98f037cbb9c1be9e7
corgan <corgan@eqdkp-plus.eu>
2010-07-09 16:07:05 +0000

    french & spanish translation

--- lastraids/language/french.php
+++ lastraids/language/french.php
@@ -1,29 +1,19 @@
 <?php
-/*
- * Project:     EQdkp-Plus
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date: 2008-07-29 00:30:30 +0200 (Di, 29 Jul 2008) $
- * -----------------------------------------------------------------------
- * @author      $Author: osr-corgan $
- * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
- * @link        http://eqdkp-plus.com
- * @package     eqdkp-plus
- * @version     $Rev: 2450 $
- * 
- * $Id: english.php 2450 2008-07-28 22:30:30Z osr-corgan $
- */
 
-if ( !defined('EQDKP_INC') ){
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$plang = array_merge($plang, array(
-  'lastraids'                 => 'Derniers raids',
-  'pk_last_raids_limit'       => 'Limite des derniers raids',
-  'pk_set_lastraids_showloot' => 'Ne pas afficher les objets en dessous ?',
-  'pk_lastraids_lootLimit'    => 'Limite des objets',
-));
+//Language: French	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 13:55
+//File: module_lastraids
+//Source-Language: english
+
+$alang = array( 
+"lastraids" => "Derniers raids",
+"pk_last_raids_limit" => "Limite des derniers raids",
+"pk_set_lastraids_showloot" => "Ne pas afficher les objets sous les derniers raids?",
+"pk_lastraids_lootLimit" => "Limite des objets",
+ );
+$plang = array_merge($plang, $alang);
 ?>
