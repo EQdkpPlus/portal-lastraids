4b52d15cd6c903f6dd4b8403f804d894443e9cc5
godmod <godmod@eqdkp-plus.eu>
2012-12-17 14:08:54 +0000

    lastraids module: create tag, use trunk folder

---
+++ language/english.php
@@ -0,0 +1,32 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2012-07-01 14:25:03 +0200 (So, 01. Jul 2012) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: godmod $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 11839 $
+ * 
+ * $Id: english.php 11839 2012-07-01 12:25:03Z godmod $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang = array(
+	'lastraids'					=> 'Last Raids',
+	'lastraids_name'			=> 'Last Raids',
+	'lastraids_desc'			=> 'Information on last raids',
+	'pk_last_raids_limit'		=> 'Last Raids Limit',
+	'pk_set_lastraids_showloot'	=> 'Don´t Show Items below the Last Raids?',
+	'pk_lastraids_lootLimit'	=> 'Limit the Items',
+	'lastraids_no_raids'		=> 'No Raids available.',
+);
+?>
