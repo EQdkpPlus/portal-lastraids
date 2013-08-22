fa61df2d0562dacf8b6af925bec50bd5e270fc69
godmod <godmod@eqdkp-plus.eu>
2013-08-22 17:02:31 +0000

    Branches for eqdkp 1.x

---
+++ language/german.php
@@ -0,0 +1,32 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2011-08-08 17:25:17 +0200 (Mo, 08. Aug 2011) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: wallenium $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 10931 $
+ * 
+ * $Id: german.php 10931 2011-08-08 15:25:17Z wallenium $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang = array(
+	'lastraids'					=> 'Letzte Raids',
+	'lastraids_name'			=> 'Letzte Raids',
+	'lastraids_desc'			=> 'Informationen der letzten Raids',
+	'pk_last_raids_limit'		=> 'Anzeige Limit der letzten Raids',
+	'pk_set_lastraids_showloot'	=> 'Loot zu den letzten Raids nicht anzeigen?',
+	'pk_lastraids_lootLimit'	=> 'Anzeige Limit der Items unter den letzten Raids',
+	'lastraids_no_raids'		=> 'Keine Raids vorhanden.',
+);
+?>
