b72bcc4a34b63386a393ceb59e1dd9aa66bd1e3e
GodMod <GodMod@users.noreply.github.com>
2015-02-10 10:27:44 +0100

    Update lastraids_portal.class.php

--- lastraids_portal.class.php
+++ lastraids_portal.class.php
@@ -28,7 +28,7 @@ class lastraids_portal extends portal_generic {
 	protected static $path		= 'lastraids';
 	protected static $data		= array(
 		'name'			=> 'LastRaids Module',
-		'version'		=> '3.0.0',
+		'version'		=> '3.0.1',
 		'author'		=> 'Corgan',
 		'icon'			=> 'fa-trophy',
 		'contact'		=> EQDKP_PROJECT_URL,
